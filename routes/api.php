dfc6f5aa03b417a755bc42f1c5dffd59f8346a41
Reza1290 <reza.muktasib@gmail.com>
2024-06-03 01:22:35 +0700

    add: setup

--- routes/api.php
+++ routes/api.php
@@ -22,51 +22,8 @@ use App\Models\Quiz;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/register', 'register');
-    Route::post('/login', 'login');
-    Route::post('/forgot', 'forgot');
-    Route::post('/users/verify-otp-forgot',  'verifyOtpForgot');
-    Route::post('/reset', 'reset');
-    Route::post('/users/verify-otp',  'verifyOtpRegister');
-    Route::post('/users/new-otp',  'newOtp');
-    Route::get('/users/profile',  'profile');
-    Route::post('/logout',  'logout');
-    Route::post('check/password', 'checkPassword');
-    Route::post('change/password', 'changePassword');
-});
-Route::get('/get-onboarding-target',[ValueHomeController::class, 'getTargetOnBoarding']);
-
 
 Route::middleware('auth:api')->group(function () {
-    Route::controller(PacketController::class)->group(function () {
-        Route::get('/get-all-paket/full-test', 'getAllPacketFullTest');
-        Route::get('/get-all-paket/mini-test', 'getAllPacketMiniTest');
-        Route::get('/get-pakets/{idPacket}', 'getQuestionPacket');
-    });
-    Route::controller(AnswerController::class)->group(function () {
-        Route::post('/submit-paket/{idPacket}', 'submitPacket');
-        Route::get('/get-score/{idPacket}', 'getScoreSubmit');
-        Route::patch('/retake-test/{idPacket}', 'retakeTest');
-        Route::get('/answer/users/{idPacket}', 'answerUsers');
-    });
-    Route::controller(BookmarkController::class)->group(function () {
-        Route::get('/get-all-bookmark', 'getAllBookmark');
-        Route::get('/get-bookmark/{idBookmark}', 'getSpesificBookmark');
-        Route::patch('/add-bookmark/{idSoal}', 'addBookmark');
-        Route::patch('/update-bookmark/{idBookmark}', 'updateBookmark');
-    });
-
-    Route::controller(ValueHomeController::class)->group(function () {
-        Route::get('/get-all/targets', 'getAllTargetValue');
-        Route::patch('/add-and-patch-target', 'addTarget');
-        Route::get('/get-score-toefl', 'getLevelUser');
-    });
-
     Route::resource('/randomword', RandomWordController::class);
     Route::resource('/quizs',QuizController::class);
     Route::resource('/quiztypes',QuizTypeController::class);
